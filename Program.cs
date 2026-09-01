c18a7cc0db0b8cb5d98a76863d4453457e1c7fed
Nik <231528278+NikW-sec@users.noreply.github.com>
2026-09-01 09:39:32 +0100

    Initialize player scores and welcome messages
    Added player score variables and welcome messages for user input.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
-﻿static void Players(string name1, string name2) //set up two players and their name (name using input later)
+static void Players(string name1, string name2) //set up two players and their name (name using input later)
 {
   string player1 = name1;
   string player2 = name2;
+  int player1Score = 0;
+  int player2Score = 0;
+  Console.WriteLine(player1);
 }  
 
 
@@ -14,10 +17,21 @@ static void TheGrid() //function for gird
     "4", "5", "6",
     "7", "8", "9"
   };
-
+  //to display grid
   Console.WriteLine($"{grid[0]} | {grid[1]} | {grid[2]}");
   Console.WriteLine("---------");
   Console.WriteLine($"{grid[3]} | {grid[4]} | {grid[5]}");
   Console.WriteLine("---------");
   Console.WriteLine($"{grid[6]} | {grid[7]} | {grid[8]}");
 }
+
+
+//inital welcomign and user name
+Console.WriteLine("Welcome to tic tac toe");
+
+Console.WriteLine("Enter the name of player 1");
+string user1 = Console.ReadLine();
+Console.WriteLine("Enter the name of player 2");
+string user2 = Console.ReadLine();
+
+Players(user1,user2);
