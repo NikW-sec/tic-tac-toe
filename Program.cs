065e3cc65e5ac549a5a2cc48042a240db568e431
Nik <231528278+NikW-sec@users.noreply.github.com>
2026-09-01 09:32:47 +0100

    Implement player setup and grid display functions
    Added functions to set up players and display the game grid.

--- Program.cs
+++ Program.cs
@@ -1,1 +1,23 @@
-﻿Console.WriteLine("Hello, World!");
+﻿static void Players(string name1, string name2) //set up two players and their name (name using input later)
+{
+  string player1 = name1;
+  string player2 = name2;
+}  
+
+
+
+static void TheGrid() //function for gird
+{
+ string[] grid =
+  {
+    "1", "2", "3",
+    "4", "5", "6",
+    "7", "8", "9"
+  };
+
+  Console.WriteLine($"{grid[0]} | {grid[1]} | {grid[2]}");
+  Console.WriteLine("---------");
+  Console.WriteLine($"{grid[3]} | {grid[4]} | {grid[5]}");
+  Console.WriteLine("---------");
+  Console.WriteLine($"{grid[6]} | {grid[7]} | {grid[8]}");
+}
