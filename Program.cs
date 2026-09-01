b7f2541c8f6a17c40d45d432564840e914535a22
joshlam67 <lamj71705@gmail.com>
2026-09-01 10:50:41 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -11,3 +11,33 @@ static int playerChoice()
         }
     }
 }
+
+
+string checkResult()
+{
+    int[,] winPatterns =
+    {
+        {0,1,2}, {3,4,5}, {6,7,8}, // row
+        {0,3,6}, {1,4,7}, {2,5,8}, // column
+        {0,4,8}, {2,4,6}          // diagonal
+    };
+
+    for (int i = 0; i < 8; i++)
+    {
+        int a = winPatterns[i, 0];
+        int b = winPatterns[i, 1];
+        int c = winPatterns[i, 2];
+
+        if (grid[a] == grid[b] && grid[b] == grid[c])
+        {
+            if (grid[a] == "X")
+            {
+                return "Player1Win";
+            }
+            else if (grid[a] == "O")
+            {
+                return "Player2Win";
+            }
+        }
+    }
+}
